73c18624d4048b4d3359c43a90d23def2c54e917
Tim Aerts <aardbeiplantje@gmail.com>
2026-02-25 09:04:08 +0100

    llama_server.ini added

--- llama_server.sh
+++ llama_server.sh
@@ -7,7 +7,7 @@ LLAMA_CPP_DIR=$(readlink -f "$LLAMA_CPP_DIR")
 
 # Your specific ROCm 7.12 build for gfx1151
 export ROCM_PATH=${ROCM_PATH?"Please set ROCM_PATH to your ROCm 7.12 build directory (e.g., ~/therock-dist-linux-gfx1151-7.12.0a20260218)"}
-export LD_LIBRARY_PATH=$ROCM_PATH/lib:$LLAMA_CPP_DIR/
+export LD_LIBRARY_PATH="$ROCM_PATH/lib:$LLAMA_CPP_DIR/"
 
 # Fix for ROCm 7.2+ hipBLASLt workspace faults on Strix Halo
 export ROCBLAS_USE_HIPBLASLT=0
@@ -23,12 +23,14 @@ if command -v numactl &> /dev/null; then
     BIND_CMD="numactl --cpunodebind=0 --membind=0"
 fi
 
+HERE="$BASH_SOURCE"
+HERE="${HERE%/*}"
 MODELS_DIR=${MODELS_DIR?"Please set MODELS_DIR to the directory where your llama.cpp models are stored (e.g., /models)"}
-LLAMA_PRESETS=${LLAMA_PRESETS?"Please set LLAMA_PRESETS to the desired model presets ini file (e.g., llama.ini)"}
+LLAMA_PRESETS="${LLAMA_PRESETS:-$HERE/llama_presets.ini}"
 cd $MODELS_DIR || exit $?
 exec $BIND_CMD $LLAMA_CPP_DIR/bin/llama-server \
-    --models-preset ${LLAMA_PRESETS} \
-    --models-dir $MODELS_DIR \
+    --models-preset "${LLAMA_PRESETS}" \
+    --models-dir "$MODELS_DIR" \
     --no-webui \
     --verbose \
     --verbose-prompt \
