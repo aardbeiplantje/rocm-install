abd520f28c4f7e010a11fbecf576c90e336cd83c
Tim Aerts <aardbeiplantje@gmail.com>
2026-03-11 14:41:27 +0100

    llama_server.sh: cleanup

--- llama_server.sh
+++ llama_server.sh
@@ -17,34 +17,35 @@ export GGML_CUDA_ENABLE_UNIFIED_MEMORY=1
 export GGML_ROCM_FORCE_TILING_ALLOCATOR=1
 export HSA_OVERRIDE_GFX_VERSION=11.5.1
 
+# Ensure the recurrent states (DeltaNet) are offloaded to the GPU
+# This is critical for Qwen3-Next performance
+export GGML_HIP_FORCE_RS_GPU=1
+export GGML_HIP_FORCE_KV_GPU=1
+export GGML_HIP_GRAPHS=0
+
+# Force a larger workspace
+export GGML_HIP_ALLOC_GRAPH_RESERVE=2048
+export ROCM_METADATA_WAIT_TIMEOUT=100
+export ROCM_ALLOW_INT8_MIXED_PRECISION=1
+
+export LLAMA_LOG_COLORS=1
+export LLAMA_LOG_TIMESTAMPS=1
+export LLAMA_LOG_PREFIX=1
+
 # Using numactl to bind to the first CCD (0-7) and its local memory. 
 # This reduces Infinity Fabric noise while the GPU is slamming the memory controller.
 BIND_CMD=""
 if command -v numactl &> /dev/null; then
     BIND_CMD="numactl --cpunodebind=0 --membind=0"
 fi
-
-# Force a larger workspace
-export GGML_HIP_ALLOC_GRAPH_RESERVE=2048
-export ROCM_METADATA_WAIT_TIMEOUT=100
-
-# Ensure the recurrent states (DeltaNet) are offloaded to the GPU
-# This is critical for Qwen3-Next performance
-export GGML_HIP_FORCE_RS_GPU=1
-export GGML_HIP_FORCE_KV_GPU=1
-export GGML_CUDA_ENABLE_UNIFIED_MEMORY=1
-export GGML_HIP_GRAPHS=0
-
-export LLAMA_LOG_COLORS=1
-export LLAMA_LOG_TIMESTAMPS=1
-export LLAMA_LOG_PREFIX=1
+# disable for now
+BIND_CMD=""
 
 HERE="$BASH_SOURCE"
 HERE="${HERE%/*}"
 MODELS_DIR=${MODELS_DIR?"Please set MODELS_DIR to the directory where your llama.cpp models are stored (e.g., /models)"}
 LLAMA_PRESETS="${LLAMA_PRESETS:-$HERE/llama_server.ini}"
 cd $MODELS_DIR || exit $?
-BIND_CMD=
 exec $BIND_CMD $LLAMA_CPP_DIR/bin/llama-server \
     --models-preset "${LLAMA_PRESETS}" \
     --models-dir "$MODELS_DIR" \
