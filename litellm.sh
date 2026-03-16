63934ab59a5a5ebdd68c46e73c9c98b558210da4
Tim Aerts <aardbeiplantje@gmail.com>
2026-03-16 13:32:31 +0100

    litellm.sh: docker added

---
+++ litellm.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+docker stop litellm || true
+ip=$(ip -6 addr show scope global|grep /128|awk '{print $2}'|cut -d/ -f1|head -n1)
+echo "Using IP: $ip"
+cat >/tmp/litellm-$LOGNAME.yaml <<EOcfg
+model_list:
+  - model_name: instruct
+    litellm_params:
+      model: openai/instruct
+      api_base: http://[$ip]:8000
+      api_key: "not-needed"
+EOcfg
+exec docker run \
+    --pull=always \
+    --name litellm \
+    --detach \
+    --rm \
+    -v /tmp/litellm-$LOGNAME.yaml:/app/config.yaml:ro \
+    -p 4000:4000 \
+    docker.litellm.ai/berriai/litellm:main-latest \
+        --port 4000 \
+        --host '::' \
+        --config /app/config.yaml \
+        --detailed_debug
